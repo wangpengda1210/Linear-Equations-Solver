ddc66a2a8f5813b415461f2a4b2e43b92a9ce714
wxy <515484505@qq.com>
2021-01-13 15:18:14 +0800

    Add problems

---
+++ Problems/Complex numbers/src/Main.java
@@ -0,0 +1,18 @@
+class ComplexNumber {
+
+    private final double re;
+    private final double im;
+
+    public ComplexNumber(double re, double im) {
+        this.re = re;
+        this.im = im;
+    }
+
+    public double getRe() {
+        return re;
+    }
+
+    public double getIm() {
+        return im;
+    }
+}
